828ad6cc7ee3e4912177dcfa5ff6e4d4ffd90dd1
w52191 <wangting52918@gmail.com>
2017-06-09 23:29:31 -0400

    Ting

--- data_storge.py
+++ data_storge.py
@@ -6,6 +6,243 @@
 get_ipython().system(u'cat ch06/ex1.csv')
 
 
+# In[46]:
+
+get_ipython().system(u'cat ch06/ex2.csv')
+
+
+# In[47]:
+
+pd.read_csv('ch06/ex2.csv', header = None)
+
+
+# In[58]:
+
+get_ipython().system(u'cat ch06/ex4.csv')
+
+
+# In[59]:
+
+pd.read_csv('ch06/ex4.csv', skiprows=[0,2,3])
+
+
+# In[60]:
+
+get_ipython().system(u'cat ch06/ex5.csv')
+
+
+# In[62]:
+
+result = pd.read_csv('ch06/ex5.csv')
+
+
+# In[63]:
+
+result
+
+
+# In[64]:
+
+pd.isnull(result)
+
+
+# In[66]:
+
+result = pd.read_csv('ch06/ex5.csv', na_values = ['NULL'])
+
+
+# In[67]:
+
+result
+
+
+# In[68]:
+
+pd.isnull(result)
+
+
+# In[71]:
+
+sentinels = {'message': ['foo', 'NA'], 'something': ['two']}
+
+
+# In[73]:
+
+pd.read_csv('ch06/ex5.csv', na_values=sentinels)
+
+
+# In[74]:
+
+pd.isnull(result)
+
+
+# In[76]:
+
+data = pd.read_csv('ch06/ex5.csv')
+
+
+# In[77]:
+
+data
+
+
+# In[78]:
+
+data.to_csv('ch06/out.csv')
+
+
+# In[79]:
+
+get_ipython().system(u'cat ch06/out.csv')
+
+
+# In[82]:
+
+import sys
+data.to_csv(sys.stdout, sep='|')
+
+
+# In[84]:
+
+data.to_csv(sys.stdout, na_rep='NULL')
+
+
+# In[85]:
+
+data.to_csv(sys.stdout, index = False, header = False)
+
+
+# In[87]:
+
+data.to_csv(sys.stdout, index=False, columns=['a','b','c'])
+
+
+# In[88]:
+
+dates = pd.date_range('1/1/2000', periods = 7)
+
+
+# In[90]:
+
+from pandas import Series
+ts = Series(np.arange(7), index=dates)
+
+
+# In[91]:
+
+ts.to_csv('ch06/tseies.csv')
+
+
+# In[92]:
+
+get_ipython().system(u'cat ch06/tseies.csv')
+
+
+# In[93]:
+
+get_ipython().system(u'cat ch06/ex7.csv')
+
+
+# In[94]:
+
+import csv
+f = open('ch06/ex7.csv')
+
+
+# In[95]:
+
+reader = csv.reader(f)
+
+
+# In[96]:
+
+for line in reader:
+    print line
+
+
+# In[97]:
+
+lines = list(csv.reader(open('ch06/ex7.csv')))
+
+
+# In[98]:
+
+header, values = lines[0],lines[1:]
+
+
+# In[99]:
+
+data_dict = {h: v for h,v in zip(header, zip(*values))}
+
+
+# In[100]:
+
+data_dict
+
+
+# In[106]:
+
+class my_dialect(csv.Dialect):
+    lineterminator = '\n'
+    delimiter = ';'
+    quotechar = '"'
+reader = csv.reader(f, dialect=my_dialect, quoting=csv.QUOTE_NONE)
+
+
+# In[108]:
+
+for line in reader:
+    print line
+
+
+# In[104]:
+
+reader = csv.reader(f, delimiter='|')
+
+
+# In[111]:
+
+with open('mydata.csv','w') as f:
+    writer = csv.writer(f)
+    writer.writerow(('one', 'two', 'three'))
+    writer.writerow(('1', '2', '3'))
+
+
+# In[48]:
+
+pd.read_csv('ch06/ex2.csv', names = ['a','b','c','d','message'])
+
+
+# In[49]:
+
+names = ['a','b','c','d','message']
+
+
+# In[51]:
+
+pd.read_csv('ch06/ex2.csv', names = names, index_col = 'message')
+
+
+# In[53]:
+
+get_ipython().system(u'cat ch06/csv_mindex.csv')
+
+
+# In[54]:
+
+parsed = pd.read_csv('ch06/csv_mindex.csv',index_col=['key1','key2'])
+
+
+# In[55]:
+
+parsed
+
+
+# In[ ]:
+
+
+
+
 # In[1]:
 
 from lxml.html import parse
